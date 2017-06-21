f1cd44af218946015852e5923f1886a2b38094d2
Scott Vaughan <scott_vaughan@hotmail.com>
2017-06-21 10:10:46 +0100

    Add new test

---
+++ Kata/Friend.cs
@@ -0,0 +1,21 @@
+﻿namespace Kata
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Friend
+    {
+        /// <summary>
+        /// Friends the or foe.
+        /// </summary>
+        /// <param name="names">The names.</param>
+        /// <returns></returns>
+        public static IEnumerable<string> FriendOrFoe(string[] names)
+        {
+            return names.Where(name => name.Length == 4).AsEnumerable();
+        }
+    }
+}
