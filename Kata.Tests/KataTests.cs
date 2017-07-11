7397a60817b2b6b977a5eb505465e85782b4716e
Scott Vaughan <scott_vaughan@hotmail.com>
2017-07-11 08:29:52 +0100

    New tests added.

---
+++ Kata.Tests/KataTests.cs
@@ -0,0 +1,61 @@
+﻿namespace Kata.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class KataTests
+    {
+        [Test]
+        public void BonusTimeTest()
+        {
+            StringAssert.AreEqualIgnoringCase("$100000", Kata.BonusTime(10000, true));
+            StringAssert.AreEqualIgnoringCase("$250000", Kata.BonusTime(25000, true));
+            StringAssert.AreEqualIgnoringCase("$10000", Kata.BonusTime(10000, false));
+            StringAssert.AreEqualIgnoringCase("$60000", Kata.BonusTime(60000, false));
+            StringAssert.AreEqualIgnoringCase("$20", Kata.BonusTime(2, true));
+            StringAssert.AreEqualIgnoringCase("$78", Kata.BonusTime(78, false));
+            StringAssert.AreEqualIgnoringCase("$678900", Kata.BonusTime(67890, true));
+        }
+
+        [Test]
+        public void MakeUpperCaseTest()
+        {
+            Assert.AreEqual("HELLO", Kata.MakeUpperCase("hello"));
+        }
+
+        [Test]
+        public void FriendOrFoeTest()
+        {
+            string[] expected = { "Ryan", "Mark" };
+            string[] names = { "Ryan", "Kieran", "Mark", "Jimmy" };
+            CollectionAssert.AreEqual(expected, Kata.FriendOrFoe(names));
+        }
+
+        [Test]
+        public void FriendOrFoeTestWithMoreNames()
+        {
+            string[] expected = { "John", "Adam" };
+            string[] names = { "John", "Scott", "Adam", "Sandra" };
+            CollectionAssert.AreEqual(expected, Kata.FriendOrFoe(names));
+        }
+
+        /// <summary>
+        /// My test.
+        /// </summary>
+        /// <param name="expectedResult">The expected result.</param>
+        /// <param name="number">The number.</param>
+        [Test]
+        [TestCase("Even", 2)]
+        [TestCase("Odd", 1)]
+        [TestCase("Even", 0)]
+        [TestCase("Odd", 7)]
+        [TestCase("Even", -2)]
+        [TestCase("Odd", -1)]
+        [TestCase("Even", -0)]
+        [TestCase("Odd", -7)]
+        public void MyTest(string expectedResult, int number)
+        {
+            Assert.AreEqual(expectedResult, Kata.EvenOrOdd(number));
+        }
+    }
+}
---
+++ Kata/Kata.cs
@@ -0,0 +1,61 @@
+﻿namespace Kata
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class Kata
+    {
+        /// <summary>
+        /// Makes the upper case.
+        /// </summary>
+        /// <param name="str">The string.</param>
+        /// <returns>Returns the string input as uppercase string.</returns>
+        public static string MakeUpperCase(string str)
+        {
+            return str.ToUpper();
+        }
+
+        /// <summary>
+        /// Bonuses the time.
+        /// </summary>
+        /// <param name="salary">The salary.</param>
+        /// <param name="bonus">if set to <c>true</c> [bonus].</param>
+        /// <returns>Returns the bonus.</returns>
+        public static string BonusTime(int salary, bool bonus)
+        {
+            return bonus ? $"${salary*10}" : $"${salary}";
+        }
+
+        /// <summary>
+        /// Friends the or foe.
+        /// </summary>
+        /// <param name="names">The names.</param>
+        /// <returns>Returns the names.</returns>
+        public static IEnumerable<string> FriendOrFoe(string[] names)
+        {
+            return names.Where(name => name.Length == 4).AsEnumerable();
+        }
+
+        /// <summary>
+        /// The even string.
+        /// </summary>
+        private const string Even = "Even";
+
+        /// <summary>
+        /// The odd string.
+        /// </summary>
+        private const string Odd = "Odd";
+
+        /// <summary>
+        /// Evens the or odd.
+        /// </summary>
+        /// <param name="number">The number.</param>
+        /// <returns>
+        /// Returns either "Odd" or "Even" for a given integer.
+        /// </returns>
+        public static string EvenOrOdd(int number)
+        {
+            return number % 2 != 0 ? Odd : Even;
+        }
+    }
+}
